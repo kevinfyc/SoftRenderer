214439a39ecac59db4b995d73cd2b24f03b79ea0
kevin <kevin>
2017-06-13 10:11:10 +0800

    2d line

--- scripts/device_context.py
+++ scripts/device_context.py
@@ -10,3 +10,29 @@ class DeviceContext:
 
 		return
 
+	# 布雷森漢姆直線演算法
+	def draw_line(self, bgn, end, color):
+		steep = abs(end.y - bgn.y) > abs(end.x - bgn.x)
+
+		if steep:
+			bgn.x, bgn.y = bgn.y, bgn.x
+			end.x, end.y = end.y, end.x
+
+		if bgn.x > end.x:
+			bgn.x, end.x = end.x, bgn.x
+			bgn.y, end.y = end.y, bgn.y
+
+		deltax = end.x - bgn.x
+		deltay = abs(end.y - bgn.y)
+		error = deltax / 2
+		y = bgn.y
+
+		ystep = 0 + bool(bgn.y < end.y)
+
+		for x in xrange(bgn.x, end.x):
+			self.device.draw_pixel(y, x, color) if steep else self.device.draw_pixel(x, y, color)
+			error = error - deltay
+
+			if error < 0:
+				y = y + ystep
+				error = error + deltax
--- scripts/main.py
+++ scripts/main.py
@@ -4,12 +4,18 @@ import sys
 
 import pygame
 
-import device
+import device, device_context
 
 from color import Color
 
 dev = device.Device(800, 600)
 dev.draw_pixel(100, 100, Color.black())
+
+# for test
+from kmath import vector2
+dev_con = device_context.DeviceContext(dev)
+dev_con.draw_line(vector2.Vector2(100, 100), vector2.Vector2(300, 500), Color.black())
+# end test
 
 while True:
 	for event in pygame.event.get():
